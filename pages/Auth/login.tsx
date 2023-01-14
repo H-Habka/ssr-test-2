d16fa95198485a0fe0377e18643d62887ad808b5
h-habka <hossien.habka1@gmail.com>
2023-01-14 16:09:59 +0200

    testing hard refresh soluation

--- pages/Auth/login.tsx
+++ pages/Auth/login.tsx
@@ -1,110 +1,121 @@
-import React, { ReactElement } from "react";
-import { useTranslation } from "react-i18next";
-import CustomCheckBox from "../../components/CustomCheckBox";
-import CustomInputField from "../../components/CustomInputField";
-import AuthLayout from "../../layouts/AuthLayout";
-import type { NextPageWithLayout } from "../_app";
-import { ImFacebook, ImTwitter, ImYoutube } from "react-icons/im";
-import { AiFillInstagram } from "react-icons/ai";
-import { useMediaQuery } from "react-responsive";
-import { api } from "../../api";
-import { useMutation } from "react-query";
-import { useForm } from "react-hook-form";
+// import React, { ReactElement } from "react";
+// import { useTranslation } from "react-i18next";
+// import CustomCheckBox from "../../components/CustomCheckBox";
+// import CustomInputField from "../../components/CustomInputField";
+// import AuthLayout from "../../layouts/AuthLayout";
+// import type { NextPageWithLayout } from "../_app";
+// import { ImFacebook, ImTwitter, ImYoutube } from "react-icons/im";
+// import { AiFillInstagram } from "react-icons/ai";
+// import { useMediaQuery } from "react-responsive";
+// import { api } from "../../api";
+// import { useMutation } from "react-query";
+// import { useForm } from "react-hook-form";
 
-const Login: NextPageWithLayout = () => {
-  const sm = useMediaQuery({
-    query: "(min-width: 640px)",
-  });
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm();
-  const { t, i18n } = useTranslation();
-  const onSubmit = (data: any) => {
-    console.log(data);
-  };
+// const Login: NextPageWithLayout = () => {
+//   const sm = useMediaQuery({
+//     query: "(min-width: 640px)",
+//   });
+//   const {
+//     register,
+//     formState: { errors },
+//     handleSubmit,
+//   } = useForm();
+//   const { t, i18n } = useTranslation();
+//   const onSubmit = (data: any) => {
+//     console.log(data);
+//   };
+//   return (
+//     <form
+//       onSubmit={handleSubmit(onSubmit)}
+//       className="relative flex flex-col items-center bg-white dark:bg-darkOne rounded-xl w-[90vw] min-w-[300px] max-w-[470px] 380:px-8 420:px-12 380:py-8 420:py-14 mt-6 py-6 px-4 mb-6 1230:mb-0 1230:mt-0 1230:h-screen 1230:justify-center 1230:rounded-none"
+//     >
+//       <div
+//         className={`absolute hidden 1230:block top-0 -translate-y-1/2 start-0  ${
+//           i18n.language.startsWith("en")
+//             ? "-translate-x-1/2"
+//             : "translate-x-1/2"
+//         }`}
+//       >
+//         <img src="/images/common/rocket.webp" alt="rocket" />
+//       </div>
+//       <p className="text-gray-700 font-bold text-2xl mb-8 dark:text-white">
+//         {t("account login")}
+//       </p>
+//       <CustomInputField
+//         type="text"
+//         label="userName or email"
+//         register={register("email")}
+//         className="my-4"
+//       />
+//       <CustomInputField
+//         type="password"
+//         label="password"
+//         register={register("password")}
+//         className="my-4"
+//       />
+//       <div className="flex justify-between  w-full mt-4">
+//         <CustomCheckBox label="remember me" />
+//         <p className="font-bold text-gray-400 cursor-pointer hover:opacity-80">
+//           {t("forget password!")}
+//         </p>
+//       </div>
+//       <button className="w-full flex items-center justify-center p-4 bg-lightOne dark:bg-darkFive text-white font-semibold mt-4 rounded-xl hover:bg-opacity-80 active:bg-opacity-95 transition-all duration-300 shadow-sm shadow-darkFive">
+//         {t("login to your account")}
+//       </button>
+//       <div className="mt-6 border-b relative w-full">
+//         <p
+//           className={`text-center font-black text-2xl 640:text-sm 640:font-medium whitespace-nowrap bg-white dark:bg-darkOne dark:text-white absolute bottom-0 translate-y-1/2 start-1/2 w-fit px-2  ${
+//             i18n.language.startsWith("en")
+//               ? "-translate-x-1/2"
+//               : "translate-x-1/2"
+//           }`}
+//         >
+//           {sm ? t("login with your social account") : t("or")}
+//         </p>
+//       </div>
+//       <div className="flex gap-2 items-center justify-between mt-8">
+//         <div
+//           title="facebook"
+//           className="transition-all duration-300 hover:-translate-y-2 cursor-pointer w-10 h-10 rounded-xl bg-[#3763D2] flex justify-center items-center"
+//         >
+//           <ImFacebook className="w-4 h-4 text-white" />
+//         </div>
+//         <div
+//           title="twitter"
+//           className="transition-all duration-300 hover:-translate-y-2 cursor-pointer w-10 h-10 rounded-xl bg-[#1ABCFF] flex justify-center items-center"
+//         >
+//           <ImTwitter className="w-4 h-4 text-white" />
+//         </div>
+//         <div
+//           title="instagram"
+//           className="transition-all duration-300 hover:-translate-y-2 cursor-pointer w-10 h-10 rounded-xl bg-[#cd486b] flex justify-center items-center"
+//         >
+//           <AiFillInstagram className="w-4 h-4 text-white" />
+//         </div>
+//         <div
+//           title="youtube"
+//           className="transition-all duration-300 hover:-translate-y-2 cursor-pointer w-10 h-10 rounded-xl bg-[#FD434F] flex justify-center items-center"
+//         >
+//           <ImYoutube className="w-4 h-4 text-white" />
+//         </div>
+//       </div>
+//     </form>
+//   );
+// };
+
+// export default Login;
+
+// Login.getLayout = function getLayout(page: ReactElement) {
+//   return <AuthLayout>{page}</AuthLayout>;
+// };
+
+
+import React from 'react'
+
+const Login = () => {
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="relative flex flex-col items-center bg-white dark:bg-darkOne rounded-xl w-[90vw] min-w-[300px] max-w-[470px] 380:px-8 420:px-12 380:py-8 420:py-14 mt-6 py-6 px-4 mb-6 1230:mb-0 1230:mt-0 1230:h-screen 1230:justify-center 1230:rounded-none"
-    >
-      <div
-        className={`absolute hidden 1230:block top-0 -translate-y-1/2 start-0  ${
-          i18n.language.startsWith("en")
-            ? "-translate-x-1/2"
-            : "translate-x-1/2"
-        }`}
-      >
-        <img src="/images/common/rocket.webp" alt="rocket" />
-      </div>
-      <p className="text-gray-700 font-bold text-2xl mb-8 dark:text-white">
-        {t("account login")}
-      </p>
-      <CustomInputField
-        type="text"
-        label="userName or email"
-        register={register("email")}
-        className="my-4"
-      />
-      <CustomInputField
-        type="password"
-        label="password"
-        register={register("password")}
-        className="my-4"
-      />
-      <div className="flex justify-between  w-full mt-4">
-        <CustomCheckBox label="remember me" />
-        <p className="font-bold text-gray-400 cursor-pointer hover:opacity-80">
-          {t("forget password!")}
-        </p>
-      </div>
-      <button className="w-full flex items-center justify-center p-4 bg-lightOne dark:bg-darkFive text-white font-semibold mt-4 rounded-xl hover:bg-opacity-80 active:bg-opacity-95 transition-all duration-300 shadow-sm shadow-darkFive">
-        {t("login to your account")}
-      </button>
-      <div className="mt-6 border-b relative w-full">
-        <p
-          className={`text-center font-black text-2xl 640:text-sm 640:font-medium whitespace-nowrap bg-white dark:bg-darkOne dark:text-white absolute bottom-0 translate-y-1/2 start-1/2 w-fit px-2  ${
-            i18n.language.startsWith("en")
-              ? "-translate-x-1/2"
-              : "translate-x-1/2"
-          }`}
-        >
-          {sm ? t("login with your social account") : t("or")}
-        </p>
-      </div>
-      <div className="flex gap-2 items-center justify-between mt-8">
-        <div
-          title="facebook"
-          className="transition-all duration-300 hover:-translate-y-2 cursor-pointer w-10 h-10 rounded-xl bg-[#3763D2] flex justify-center items-center"
-        >
-          <ImFacebook className="w-4 h-4 text-white" />
-        </div>
-        <div
-          title="twitter"
-          className="transition-all duration-300 hover:-translate-y-2 cursor-pointer w-10 h-10 rounded-xl bg-[#1ABCFF] flex justify-center items-center"
-        >
-          <ImTwitter className="w-4 h-4 text-white" />
-        </div>
-        <div
-          title="instagram"
-          className="transition-all duration-300 hover:-translate-y-2 cursor-pointer w-10 h-10 rounded-xl bg-[#cd486b] flex justify-center items-center"
-        >
-          <AiFillInstagram className="w-4 h-4 text-white" />
-        </div>
-        <div
-          title="youtube"
-          className="transition-all duration-300 hover:-translate-y-2 cursor-pointer w-10 h-10 rounded-xl bg-[#FD434F] flex justify-center items-center"
-        >
-          <ImYoutube className="w-4 h-4 text-white" />
-        </div>
-      </div>
-    </form>
-  );
-};
+    <div>Login</div>
+  )
+}
 
-export default Login;
-
-Login.getLayout = function getLayout(page: ReactElement) {
-  return <AuthLayout>{page}</AuthLayout>;
-};
+export default Login
--- pages/Auth/signup.tsx
+++ pages/Auth/signup.tsx
@@ -1,89 +1,101 @@
-import React, { ReactElement } from "react";
-import { useTranslation } from "react-i18next";
-import CustomCheckBox from "../../components/CustomCheckBox";
-import CustomInputField from "../../components/CustomInputField";
-import AuthLayout from "../../layouts/AuthLayout";
-import type { NextPageWithLayout } from "../_app";
-import { useMutation } from "react-query";
-import { api } from "../../api";
-import { useForm } from "react-hook-form";
+// import React, { ReactElement } from "react";
+// import { useTranslation } from "react-i18next";
+// import CustomCheckBox from "../../components/CustomCheckBox";
+// import CustomInputField from "../../components/CustomInputField";
+// import AuthLayout from "../../layouts/AuthLayout";
+// import type { NextPageWithLayout } from "../_app";
+// import { useMutation } from "react-query";
+// import { api } from "../../api";
+// import { useForm } from "react-hook-form";
 
-const Signup: NextPageWithLayout = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const { mutate, isLoading, error } = useMutation(api.auth.register);
-  const { t, i18n } = useTranslation();
-  const onSubmit = (data: any) => {
-    mutate(data);
-  };
+// const Signup: NextPageWithLayout = () => {
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm();
+//   const { mutate, isLoading, error } = useMutation(api.auth.register);
+//   const { t, i18n } = useTranslation();
+//   const onSubmit = (data: any) => {
+//     mutate(data);
+//   };
 
+//   return (
+//     <form
+//       onSubmit={handleSubmit(onSubmit)}
+//       className="flex flex-col items-center bg-white dark:bg-darkOne rounded-xl w-[90vw] min-w-[300px] max-w-[470px] 380:px-8 420:px-12 380:py-8 420:py-10 mt-6 py-6 px-4 mb-6 1230:py-4 1230:rounded-none relative 1230:h-screen 1230:justify-center"
+//     >
+//       <div
+//         className={`absolute hidden 1230:block top-0 -translate-y-1/2 start-0 ${
+//           i18n.language.startsWith("en")
+//             ? "-translate-x-1/2"
+//             : "translate-x-1/2"
+//         }`}
+//       >
+//         <img src="/images/common/rocket.webp" alt="rocket" />
+//       </div>
+//       <p className="text-gray-700 font-bold text-2xl mb-8 dark:text-white">
+//         {t("create your account")}
+//       </p>
+//       <CustomInputField
+//         type="text"
+//         label="email"
+//         register={{ ...register("email") }}
+//         className="my-3"
+//       />
+//       <CustomInputField
+//         type="text"
+//         label="userName"
+//         register={{ ...register("userName") }}
+//         className="my-3"
+//       />
+//       <CustomInputField
+//         type="password"
+//         label="password"
+//         register={{ ...register("password") }}
+//         className="my-3"
+//       />
+//       <CustomInputField
+//         type="password"
+//         label="repeat password"
+//         register={{ ...register("confirmPassword") }}
+//         className="my-3"
+//       />
+//       <div className="flex justify-start  w-full">
+//         <CustomCheckBox label="send me news and updates via email" />
+//       </div>
+//       <button className="w-full flex items-center justify-center p-4 bg-lightTwo dark:bg-darkThree text-white font-semibold mt-4 rounded-xl hover:bg-opacity-80 active:bg-opacity-95 transition-all duration-300">
+//         <p>{isLoading ? t("loading ...") : t("register now!")}</p>
+//       </button>
+//       <div className="font-medium mt-8 w-full text-start text-sm dark:text-white">
+//         {t(
+//           "you will receive a confirmation email in your inbox with a link to activate your account"
+//         )}
+//         {". "}
+//         {t("if you have any problems")}
+//         {", "}
+//         <span className="text-lightTwo dark:text-darkFour font-semibold cursor-pointer hover:opacity-80 transition-all duration-300">
+//           {t("contact us")}
+//         </span>
+//       </div>
+//     </form>
+//   );
+// };
+
+// export default Signup;
+
+// Signup.getLayout = function getLayout(page: ReactElement) {
+//   return <AuthLayout>{page}</AuthLayout>;
+// };
+
+
+
+import React from 'react'
+
+const SignUp = () => {
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col items-center bg-white dark:bg-darkOne rounded-xl w-[90vw] min-w-[300px] max-w-[470px] 380:px-8 420:px-12 380:py-8 420:py-10 mt-6 py-6 px-4 mb-6 1230:py-4 1230:rounded-none relative 1230:h-screen 1230:justify-center"
-    >
-      <div
-        className={`absolute hidden 1230:block top-0 -translate-y-1/2 start-0 ${
-          i18n.language.startsWith("en")
-            ? "-translate-x-1/2"
-            : "translate-x-1/2"
-        }`}
-      >
-        <img src="/images/common/rocket.webp" alt="rocket" />
-      </div>
-      <p className="text-gray-700 font-bold text-2xl mb-8 dark:text-white">
-        {t("create your account")}
-      </p>
-      <CustomInputField
-        type="text"
-        label="email"
-        register={{ ...register("email") }}
-        className="my-3"
-      />
-      <CustomInputField
-        type="text"
-        label="userName"
-        register={{ ...register("userName") }}
-        className="my-3"
-      />
-      <CustomInputField
-        type="password"
-        label="password"
-        register={{ ...register("password") }}
-        className="my-3"
-      />
-      <CustomInputField
-        type="password"
-        label="repeat password"
-        register={{ ...register("confirmPassword") }}
-        className="my-3"
-      />
-      <div className="flex justify-start  w-full">
-        <CustomCheckBox label="send me news and updates via email" />
-      </div>
-      <button className="w-full flex items-center justify-center p-4 bg-lightTwo dark:bg-darkThree text-white font-semibold mt-4 rounded-xl hover:bg-opacity-80 active:bg-opacity-95 transition-all duration-300">
-        <p>{isLoading ? t("loading ...") : t("register now!")}</p>
-      </button>
-      <div className="font-medium mt-8 w-full text-start text-sm dark:text-white">
-        {t(
-          "you will receive a confirmation email in your inbox with a link to activate your account"
-        )}
-        {". "}
-        {t("if you have any problems")}
-        {", "}
-        <span className="text-lightTwo dark:text-darkFour font-semibold cursor-pointer hover:opacity-80 transition-all duration-300">
-          {t("contact us")}
-        </span>
-      </div>
-    </form>
-  );
-};
+    <div>SignUp</div>
+  )
+}
 
-export default Signup;
-
-Signup.getLayout = function getLayout(page: ReactElement) {
-  return <AuthLayout>{page}</AuthLayout>;
-};
+export default SignUp
